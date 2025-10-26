075deba9ecb1a88f17a307b63873b3fe64ee64cf
blink1197 <cgbagotsay11@gmail.com>
2025-10-26 15:02:37 +0800

    Add DOMContentLoaded to properly run the js script when the page is fully loaded

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,36 +4,38 @@ console.log('Hello World!');
 // MOCK LOGIN LOGIC (for frontend testing only)
 // =========================================================================
 
-const loginForm = document.getElementById('loginForm');
-const bypassButton = document.getElementById('bypassButton'); // Get the new button
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginForm');
+    const bypassButton = document.getElementById('bypassButton'); // Get the new button
 
-if (loginForm) {
-    loginForm.addEventListener('submit', function(event) {
+    if (loginForm) {
+        loginForm.addEventListener('submit', function (event) {
 
-        event.preventDefault(); 
+            event.preventDefault();
+            const emailInput = document.getElementById('emailInput').value;
+            const passwordInput = document.getElementById('passwordInput').value;
 
-        const emailInput = document.getElementById('emailInput').value;
-        const passwordInput = document.getElementById('passwordInput').value;
+            const ADMIN_EMAIL = 'admin@example.com';
+            const ADMIN_PASSWORD = 'admin123';
 
-        const ADMIN_EMAIL = 'admin@example.com';
-        const ADMIN_PASSWORD = 'admin123';
-        
-        if (emailInput === ADMIN_EMAIL && passwordInput === ADMIN_PASSWORD) {
-            alert('Admin login successful! Redirecting to User Profile (PG-11).');
-            window.location.href = './profile.html'; 
-        } else {
-            alert('Login failed. Incorrect credentials. Use admin@example.com / admin123 to test the profile page, or click the Bypass button.');
-        }
-    });
-}
+            if (emailInput === ADMIN_EMAIL && passwordInput === ADMIN_PASSWORD) {
+                alert('Admin login successful! Redirecting to User Profile (PG-11).');
+                window.location.href = './profile.html';
+            } else {
+                alert('Login failed. Incorrect credentials. Use admin@example.com / admin123 to test the profile page, or click the Bypass button.');
+            }
+        });
+    }
 
-if (bypassButton) {
-    bypassButton.addEventListener('click', function() {
+    if (bypassButton) {
+        bypassButton.addEventListener('click', function () {
 
-        alert('Bypass activated. Redirecting to User Profile Page.');
-        window.location.href = './profile.html';
-    });
-}
+            alert('Bypass activated. Redirecting to User Profile Page.');
+            window.location.href = './profile.html';
+        });
+    }
+})
+
 
 // =========================== end ========================================================
 // ========================================================================================
