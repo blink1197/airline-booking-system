74b522f1ef8116ceceae2f567578f58dd11dabd8
blink1197 <cgbagotsay11@gmail.com>
2025-11-01 00:32:31 +0800

    Upddate navbar scrolling

--- src/components/layout/NavbarComponent.vue
+++ src/components/layout/NavbarComponent.vue
@@ -6,12 +6,14 @@
           <img class="img-fluid" src="../../assets/images/logo_dark.png" alt="Flyx Logo" height="44">
         </router-link>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" @click="toggleNavbar" aria-controls="navbarNavAltMarkup"
+          :aria-expanded="visible.toString()" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <!-- Smooth height transition -->
+        <div ref="menu" class="navbar-collapse" :class="{ show: visible }" :style="{ height: menuHeight }"
+          id="navbarNavAltMarkup">
           <div class="navbar-nav ms-auto gap-3">
             <router-link class="nav-link" to="/">Home</router-link>
             <router-link class="nav-link" to="/about">About</router-link>
@@ -24,20 +26,39 @@
     </div>
   </nav>
 </template>
+
+<script setup>
+import { nextTick, ref } from 'vue'
+
+const visible = ref(false)
+const menu = ref(null)
+const menuHeight = ref('0px')
+
+function toggleNavbar() {
+  visible.value = !visible.value
+  nextTick(() => {
+    if (visible.value) {
+      menuHeight.value = menu.value.scrollHeight + 'px' // expand smoothly
+    } else {
+      menuHeight.value = '0px' // collapse smoothly
+    }
+  })
+}
+</script>
+
 <style scoped>
-nav {
-  color: var(--color-primary) !important;
-}
-
-nav .logo-text {
-  color: var(--color-primary) !important;
-}
-
 nav .nav-link {
   color: var(--color-gray-3) !important;
+  transition: color 0.3s ease;
 }
 
 nav .nav-link:hover {
   color: var(--color-primary) !important;
+}
+
+/* Smooth height animation */
+.navbar-collapse {
+  overflow: hidden;
+  transition: height 0.3s ease;
 }
 </style>
