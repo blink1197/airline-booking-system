ddbf4d2c2b859d942a2c9985f55fc80058d5d643
blink1197 <cgbagotsay11@gmail.com>
2025-11-01 00:33:52 +0800

    Fix syntax error on setting the state persistence

--- src/stores/flightSearch.js
+++ src/stores/flightSearch.js
@@ -1,49 +1,52 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useFlightSearchStore = defineStore('flightSearch', () => {
-  // --- State ---
-  const tripType = ref('oneWayTrip')
-  const from = ref(null)
-  const to = ref(null)
-  const departureDate = ref('')
-  const returnDate = ref('')
-  const pax = ref({ adults: 1, children: 0, infants: 0 });
-  const cabin = ref('Economy');
+export const useFlightSearchStore = defineStore(
+  'flightSearch', // store id
+  () => {
+    // --- State ---
+    const tripType = ref('oneWayTrip')
+    const from = ref(null)
+    const to = ref(null)
+    const departureDate = ref('')
+    const returnDate = ref('')
+    const pax = ref({ adults: 1, children: 0, infants: 0 })
+    const cabin = ref('Economy')
 
-  // --- Actions ---
-  function setFlightSearchData(data) {
-    tripType.value = data.tripType || tripType.value
-    from.value = data.from || from.value
-    to.value = data.to || to.value
-    departureDate.value = data.departureDate || departureDate.value
-    returnDate.value = data.returnDate || returnDate.value
-    pax.value = data.pax || pax.value
-    cabin.value = data.cabin || cabin.value
-  }
+    // --- Actions ---
+    function setFlightSearchData(data) {
+      tripType.value = data.tripType || tripType.value
+      from.value = data.from || from.value
+      to.value = data.to || to.value
+      departureDate.value = data.departureDate || departureDate.value
+      returnDate.value = data.returnDate || returnDate.value
+      pax.value = data.pax || pax.value
+      cabin.value = data.cabin || cabin.value
+    }
 
-  function resetFlightSearch() {
-    tripType.value = 'oneWayTrip'
-    from.value = ''
-    to.value = ''
-    departureDate.value = ''
-    returnDate.value = ''
-    pax.value = ''
-    cabin.value = ''
-  }
+    function resetFlightSearch() {
+      tripType.value = 'oneWayTrip'
+      from.value = null
+      to.value = null
+      departureDate.value = ''
+      returnDate.value = ''
+      pax.value = { adults: 1, children: 0, infants: 0 }
+      cabin.value = 'Economy'
+    }
 
-  return {
-    tripType,
-    from,
-    to,
-    departureDate,
-    returnDate,
-    pax,
-    cabin,
-    setFlightSearchData,
-    resetFlightSearch,
+    return {
+      tripType,
+      from,
+      to,
+      departureDate,
+      returnDate,
+      pax,
+      cabin,
+      setFlightSearchData,
+      resetFlightSearch,
+    }
   },
   {
-    persist: true, // Enable localStorage persistence
+    persist: true, // Persist state in localStorage
   }
-})
+)
