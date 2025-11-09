2199ca92456d9699e7ed2e920a7276b822315eb3
blink1197 <cgbagotsay11@gmail.com>
2025-11-10 02:09:01 +0800

    add a bookflight function

--- src/stores/booking.js
+++ src/stores/booking.js
@@ -1,26 +1,67 @@
+import api from '@/api/api'
 import { defineStore } from 'pinia'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { useAddonsStore } from './add-ons'
 import { useContactStore } from './contact'
+import { useFlightSearchStore } from './flightSearch'
 import { usePassengersStore } from './passengers'
 
 export const useBookingStore = defineStore('booking', () => {
-  const passengersStore = usePassengersStore()
-  const contactStore = useContactStore()
+  const flightStore = useFlightSearchStore();
+  const passengersStore = usePassengersStore();
+  const contactStore = useContactStore();
+  const addOnsStore = useAddonsStore();
+  const isBooking = ref(false);
+  const bookingError = ref(null);
 
+  const bookedFlightDetails = ref(null)
 
   const bookingData = computed(() => ({
+    flightId: flightStore.selectedFlight._id,
     passengers: passengersStore.passengers,
-    contact: contactStore.contact
+    cabin: flightStore.cabin,
+    bookingContact: contactStore.contact,
+    addOns: Object.values(addOnsStore.addOns).filter(a => a.selected)
+      .map(a => ({ name: a.addOnName }))
   }))
+
+  async function bookFlight() {
+    try {
+      isBooking.value = true;
+      bookingError.value = null;
+      const response = await api.post('/bookings', bookingData.value);
+      bookedFlightDetails.value = response.data.booking
+      console.log(bookedFlightDetails)
+      return true;
+    } catch (err) {
+      console.error("Error booking flight:", err);
+      bookingError.value = err.response?.data?.message || "Failed to book flight";
+      return false;
+    } finally {
+      isBooking.value = false;
+    }
+  }
 
 
   function resetBooking() {
-    passengersStore.resetPassengers()
-    contactStore.resetContact()
+    passengersStore.resetPassengers();
+    contactStore.resetContact();
+    addOnsStore.resetAddOns();
+    flightStore.clearSelectedFlight();
+    flightStore.resetFlightSearch();
   }
 
   return {
     bookingData,
+    bookFlight,
+    bookedFlightDetails,
+    isBooking,
+    bookingError,
     resetBooking
   }
-})
+},
+  {
+    persist: false
+  }
+
+)
