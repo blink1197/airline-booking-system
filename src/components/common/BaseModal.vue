a36349904adc4f67644cddb7ff3c3bbf4132c643
blink1197 <cgbagotsay11@gmail.com>
2025-11-14 04:18:37 +0800

    Ad jusst styling of modal title and change the default value

--- src/components/common/BaseModal.vue
+++ src/components/common/BaseModal.vue
@@ -4,7 +4,7 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="modalLabel">{{ title }}</h5>
+          <h5 class="modal-title fw-bold" id="modalLabel">{{ title }}</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -32,7 +32,7 @@ import { defineExpose, onMounted, ref } from 'vue'
 const props = defineProps({
   title: {
     type: String,
-    default: 'Filter and sort'
+    default: 'Modal title'
   }
 })
 
